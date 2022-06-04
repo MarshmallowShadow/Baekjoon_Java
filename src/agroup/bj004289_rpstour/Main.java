1dbdb76dcd6f56d3b53af9222fb6c5cca550bf29
MarshmallowShadow <choiboseung@naver.com>
2022-06-04 14:34:20 +0900

    2022-06-04

--- src/agroup/bj004289_rpstour/Main.java
+++ src/agroup/bj004289_rpstour/Main.java
@@ -34,53 +34,62 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while(true) {
-			int n = sc.nextInt();
+			int n = sc.nextInt(); //선수
 			
+			//0으로 반복문 끊김
 			if(n == 0) {
 				break;
 			}
 			
-			int k = sc.nextInt();
+			int k = sc.nextInt(); //각 게임
 			sc.nextLine();
 			
 			double[] wins = new double[n];
 			double[] total = new double[n];
 			
-			int games = k*n*(n-1)/2;
+			int games = k*n*(n-1)/2; //총 진행되는 게임
 			
 			for(int j=0; j<games; j++) {
-				int p1 = sc.nextInt();
-				String m1 = sc.next();
-				int p2 = sc.nextInt();
-				String m2 = sc.next();
+				int p1 = sc.nextInt(); //선수1 번호
+				String m1 = sc.next(); //선수1 입력
+				int p2 = sc.nextInt(); //선수2 번호
+				String m2 = sc.next(); //선수2 입력
 				sc.nextLine();
 				
-				int winner = rps(p1, m1, p2, m2);
+				int winner = rps(p1, m1, p2, m2); //어느 전수가 이기는가
 				
+				//무승부 아니면
 				if(winner != -1) {
 					wins[(int)winner-1]++;
 					
+					//승패가 가릴 경우에만 총 진행항 게임 횟수에 추가
 					total[p1-1]++;
 					total[p2-1]++;
 				}
 			}
 			
+			//선수들 승률 출력
 			for(int i=0; i<n; i++) {
+				//선수가 그냥 무승부만 있을 경우엔 "-" 출력
 				if((int)total[i] == 0) {
 					System.out.println("-");
 					continue;
 				}
+				
+				//아니면 3반올림으로 출력
 				double rate = wins[i]/total[i];
-				System.out.format("%.3f", rate);
+				System.out.format("%.3f", rate); //반올림
 				System.out.println();
 			}
 			
+			//"Output an empty line between cases."
 			System.out.println();
 		}
 		
 		sc.close();
 	}
 	
+	//가위바위보
 	public static int rps(int p1, String m1, int p2, String m2) {
 		if(m1.equals("rock") && m2.equals("scissors")) {
 			return p1;
@@ -94,7 +103,7 @@ public class Main {
 			return p2;
 		} else if(m2.equals("scissors") && m1.equals("paper")) {
 			return p2;
-		} else {
+		} else { //무승부면 -1
 			return -1;
 		}
 	}
