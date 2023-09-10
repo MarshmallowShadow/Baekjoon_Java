72930a2ff5951ed6097d077fd5aa2a5499ef406d
MarshmallowShadow <choiboseung@naver.com>
2023-09-10 13:37:45 +0900

    2023-09-10

--- src/bj001014_nocheatingsmall/Main.java
+++ src/bj001014_nocheatingsmall/Main.java
@@ -19,8 +19,11 @@ package bj001014_nocheatingsmall;
 출력: 각각의 테스트 케이스에 대해 그 교실에서 시험을 볼 수 있는 최대 학생의 수를 출력한다.
 
 */
-import java.io.*;
-import java.util.StringTokenizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -31,20 +34,96 @@ public class Main {
 			StringTokenizer input = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(input.nextToken());
 			int M = Integer.parseInt(input.nextToken());
-			String[][] classroom = new String[N][M];
-			
+			int[][] seats = new int[N][M];
 			for(int j=0; j<N; j++) {
-				classroom[j] = br.readLine().split("");
+				String row = br.readLine();
+				for(int k=0; k<M; k++) {
+					if(row.charAt(k) == '.') {
+						seats[j][k] = 1;
+					}
+				}
 			}
-
-			System.out.println(maxSeats(classroom));
+			
+			//BFS
+			int maxCount = 0;
+			for(int j=0; j<N; j++) {
+				for(int k=0; k<M; k++) {
+					if(seats[j][k] > 0) {
+						List<List<String>> count = new ArrayList<>();
+						count.add(new ArrayList<>());
+						
+						count.get(0).add(j + "," + k);
+						seats[j][k] = 0;
+						
+						match(j, k, 0, seats, count);
+						
+						int[] dp = new int[count.size()];
+						int dpCount = 0;
+						
+						for(int l=0; l<dp.length; l++) {
+							//TEST ERASE ERASE ERASE ERASE ERASE ERASE ERASE ERASE ERASE ERASE ERASE ERASE ERASE ERASE ERASE 
+							//System.out.println(count.get(l));
+							
+							dp[l] = count.get(l).size();
+							
+							if (l==2) {
+								dp[l] += dp[l-2];
+							} else if(l > 2) {
+								dp[l] += (dp[l-3] > dp[l-2]) ? dp[l-3] : dp[l-2];
+							}
+							
+							if(dpCount < dp[l]) dpCount = dp[l];
+						}
+						
+						maxCount += dpCount;
+					}
+				}
+			}
+			
+			
+			System.out.println(maxCount);
 		}
 		
 		br.close();
 	}
 	
-	static int maxSeats(String[][] classroom) {
-		return 0;
+	//Find All Matching Pairs
+	static void match( int posY, int posX, int group, int[][] seats, List<List<String>> count) {
+		if(posX < 0 || posY < 0 || posX >= seats[0].length || posY >= seats.length) return;
+		
+		List<List<Integer>> posList = new ArrayList<>();
+		
+		for(int i=-1; i<=1; i++) {
+			if(posX + 1 < seats[0].length && posY + i >= 0 && posY + i < seats.length) {
+				if(seats[posY+i][posX+1] != 0) {
+					posList.add(new ArrayList<>());
+					posList.get(posList.size()-1).add(posY+i);
+					posList.get(posList.size()-1).add(posX+1);
+					
+					if(count.size() == group + 1) count.add(new ArrayList<>());
+					String pos = (posY+i) + "," + (posX+1);
+					count.get(group + 1).add(pos);
+					seats[posY+i][posX+1] = 0;
+				}
+			}
+			
+			if(posX - 1 >= 0 && posY + i >= 0 && posY + i < seats.length) {
+				if(seats[posY+i][posX-1] != 0) {
+					posList.add(new ArrayList<>());
+					posList.get(posList.size()-1).add(posY+i);
+					posList.get(posList.size()-1).add(posX-1);
+					
+					if(count.size() == group + 1) count.add(new ArrayList<>());
+					String pos = (posY+i) + "," + (posX-1);
+					count.get(group + 1).add(pos);
+					seats[posY + i][posX - 1] = 0;
+				}
+			}
+		}
+		
+		for(int i=0; i<posList.size(); i++) {
+			match(posList.get(i).get(0), posList.get(i).get(1), group+1, seats, count);
+		}
 	}
 }
 
@@ -54,22 +133,197 @@ public class Main {
 5 5
 x...x
 ...x.
-xxxxx
-.x..x
+x..xx
+.xx.x
 ..x.x
-
 Answer: 8
 
-x...x
-.x.x.
-x.x.x
-xxx.x
-x.x.x
+1
+5 8
+.X...X..
+..X.....
+X.......
+.X......
+..X.....
+Answer: 18
+
+1
+10 10
+.X.X...X..
+.X..X.....
+X.X.......
+.X.X......
+X...X.....
+.X.X...X..
+.X..X.....
+X.X.......
+.X.X......
+X...X.....
+Answer: 42
+
+1
+5 10
+.X.X...X..
+.X..X.....
+X.X.......
+.X.X......
+X...X.....
+Answer: 21
+
+1
+5 7
+X...X..
+.X.....
+.......
+X......
+.X.....
+Answer: 17
+
+1
+3 3
+xx.
+xxx
+x.x
+Answer: 2
 
 
-Notes:
-if comparison ends up same, prioritize next ones (delet current chair)
-2 Cases for Each Seat (count 1 and count 2)
+<<<<<INPUT>>>>>
+17
+2 3
+...
+...
+2 3
+x.x
+xxx
+2 3
+x.x
+x.x
+10 10
+....x.....
+..........
+..........
+..x.......
+..........
+x...x.x...
+.........x
+...x......
+........x.
+.x...x....
+10 10
+xxxxxxxxxx
+xxxxxxxxxx
+xxxxxxxxxx
+xxxxxxxxxx
+xxxxxxxxxx
+xxxxxxxxxx
+xxxxxxxxxx
+xxxxxxxxxx
+xxxxxxxxxx
+xxxxxxxxxx
+3 3
+xx.
+xxx
+x.x
+3 5
+..x..
+..x..
+.xxx.
+1 6
+..x...
+10 10
+.x.x...x..
+.x..x.....
+x.x.......
+.x.x......
+x...x.....
+.x.x...x..
+.x..x.....
+x.x.......
+.x.x......
+x...x.....
+5 10
+.x.x...x..
+.x..x.....
+x.x.......
+.x.x......
+x...x.....
+5 8
+.x...x..
+..x.....
+x.......
+.x......
+..x.....
+5 7
+x...x..
+.x.....
+.......
+x......
+.x.....
+4 3
+x.x
+x..
+.x.
+.xx
+3 4
+.x.x
+.xx.
+x.x.
+5 4
+x.x.
+.x.x
+x.x.
+x..x
+..xx
+10 10
+.x.x...x..
+.x..x.....
+x.xx..x...
+.x.x....x.
+x...x.x...
+.x.x...x..
+.x..x...x.
+x.x.......
+.x.x......
+x.........
+10 10
+xxxxxxxxxx
+..........
+..........
+xxxxxxxxxx
+..........
+xxxxxxxxxx
+.........x
+...x......
+........x.
+xxxxxxxxxx
 
+<<OUTPUT>>
+4
+1
+2
+46
+0
+2
+6
+3
+42
+21
+18
+17
+4
+4
+6
+42
+29
 
+<<CRITICAL ERROR>>
+1
+6 4
+x.x.
+x...
+x.x.
+.x.x
+...x
+xx.x
+Answer: 7
 */
